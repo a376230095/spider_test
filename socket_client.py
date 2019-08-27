591dc702e6e1e22eed08fdf5f45e44d791f67709
ZengZhitong <376230095@qq.com>
2019-08-28 00:32:07 +0800

    fff

--- socket_client.py
+++ socket_client.py
@@ -1,5 +1,12 @@
 import socket
 client=socket.socket()
 client.connect(("192.168.66.158",8000))
-client.send("tongtong".encode("utf-8"))
-client.close()
+server_data = client.recv(1024)
+print("server_date: {}".format(server_data.decode("utf8")))
+while True:
+    input_str=input()
+    #if input_str!="q":
+    client.send(input_str.encode("utf8"))
+    server_data=client.recv(1024)
+    print("server_date: {}".format(server_data.decode("utf8")))
+#client.close()
--- socket_server.py
+++ socket_server.py
@@ -3,9 +3,19 @@ server=socket.socket()
 server.bind(("0.0.0.0",8000))
 server.listen()
 sock,addr=server.accept()
-print(addr)
-data=sock.recv(1024).decode("utf8")
-print("----------------")
+data=""
+while True:
+    sock.send("welcome to server!".encode("utf8"))
+    tmp_data=sock.recv(1024).decode("utf8")
+    print(tmp_data)
+    input_date=input()
+    sock.send(input_date.encode("utf8"))
+
+    # if tmp_data:
+    #     data+=tmp_data
+    #     if tmp_data.endswith("#"):
+    #         break
+    # else:
+    #     break
 print(data)
-print("----------------")
 server.close()
