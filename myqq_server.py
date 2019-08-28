20941b76a9b675c3052b47d9762085730c45068d
ZengZhitong <376230095@qq.com>
2019-08-28 23:47:45 +0800

    ffffd

--- myqq_server.py
+++ myqq_server.py
@@ -6,14 +6,15 @@
 import socket
 from collections import defaultdict
 import threading
+import json
 
-#defaultdict的作用可以让我们随便创建一个新的字典，就算key不存在也行
-a=defaultdict(dict)
-a=dict()
-a["a"]["b"]="c"
-print(a)  #defaultdict(<class 'dict'>, {'a': {'b': 'c'}})
-b=dict()
-print(b["a"]) #这样就会报错了，因为没有默认值了
+# #defaultdict的作用可以让我们随便创建一个新的字典，就算key不存在也行
+# a=defaultdict(dict)
+# a=dict()
+# a["a"]["b"]="c"
+# print(a)  #defaultdict(<class 'dict'>, {'a': {'b': 'c'}})
+# b=dict()
+# print(b["a"]) #这样就会报错了，因为没有默认值了
 
 #1.维护用户连接
 online_user=defaultdict(dict)
@@ -25,3 +26,12 @@ server=socket.socket()
 #绑定ip
 server.bind(("0.0.0.0",8000))
 server.listen()
+
+def handle_sock(sock,addr):
+    pass
+
+while True:
+    #阻塞等待连接
+    socket,addr =server.accept()
+    #启动一个线程去处理新的用户连接
+    client_threading=threading.Thread(target=handle_sock,args=(sock,addr))
